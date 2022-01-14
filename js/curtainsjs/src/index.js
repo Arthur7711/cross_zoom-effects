fc400595e5a70646cadf61fad0350af46e249c8f
Arthur <newartroll11@gmail.com>
2022-01-14 17:26:43 +0400

    feat:(cross zoom effects)

---
+++ js/curtainsjs/src/index.js
@@ -0,0 +1,21 @@
+// core
+export {Curtains} from 'curtainsjs/src/core/Curtains';
+export {Plane} from 'curtainsjs/src/core/Plane';
+export {Texture} from 'curtainsjs/src/core/Texture';
+
+// frame buffer objects
+export {RenderTarget} from 'curtainsjs/src/framebuffers/RenderTarget';
+export {ShaderPass} from 'curtainsjs/src/framebuffers/ShaderPass';
+
+// loader
+export {TextureLoader} from 'curtainsjs/src/loaders/TextureLoader';
+
+// math
+export {Vec2} from 'curtainsjs/src/math/Vec2';
+export {Vec3} from 'curtainsjs/src/math/Vec3';
+export {Mat4} from 'curtainsjs/src/math/Mat4';
+
+// extras
+// TODO TextTexture class?
+export {PingPongPlane} from 'curtainsjs/src/extras/PingPongPlane';
+export {FXAAPass} from 'curtainsjs/src/extras/FXAAPass';
